13d0a0ab2bdf1948dcaa35029b00552b6449dac9
Alejandro Trikitrakatelas Sanchez Cano <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2022-11-20 19:04:03 -0600

    Se añade el Drag and Drop del archivo
    Esta es la primera versión por lo que lo demás para el crud aún no existe

---
+++ public/JS/drag_and_drop.js
@@ -0,0 +1,55 @@
+const dragArea = document.querySelector('.drag-area');
+const dragText = document.querySelector('.headerD');
+
+let file;
+
+
+//cuando el archibo está en el drag area
+
+dragArea.addEventListener('dragover', (event) =>{
+    event.preventDefault();
+    dragText.textContent = 'Preparado para subir'
+    dragArea.classList.add('active')
+    //console.log('file is inside the drag area');
+});
+
+// cuando el archivo deja el drag area
+
+dragArea.addEventListener('dragleave', () =>{
+    //console.log('dejó el area');
+    dragText.textContent = 'Arrastra y suelta'
+    dragArea.classList.remove('active')
+
+});
+
+// Cuando es droppeado al contador
+
+dragArea.addEventListener('drop', (event) =>{
+    event.preventDefault();
+
+    file = event.dataTransfer.files[0];
+    //console.log(file);
+
+    let fileType = file.type;
+    
+    //console.log(fileType);
+    //console.log("Archivo dejado");
+
+    let validExtensions = ['image/jpeg', 'image/jpg', 'image/png'];
+
+    if(validExtensions.includes(fileType)){
+        let fileReader = new FileReader();
+
+        fileReader.onload = ()=>{
+
+            let fileURL = fileReader.result;
+            //console.log(fileURL)
+            let imgTag = `<img src = "${fileURL}" alt="">`;
+            dragArea.innerHTML = imgTag;
+        }
+        fileReader.readAsDataURL(file);
+    }else{
+        alert('Este Archivo no es una imagen');
+        dragArea.classList.remove('active');
+    }
+})
