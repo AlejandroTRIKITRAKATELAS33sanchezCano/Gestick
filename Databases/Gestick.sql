f027505cabaab03d6f9c5e7d23e6141c230ab14f
ADVMCA <cvazquezm2002@alumno.ipn.mx>
2022-11-22 17:42:10 -0600

    Version final

--- Databases/Gestick.sql
+++ Databases/Gestick.sql
@@ -3,7 +3,6 @@
 -- Model: New Model    Version: 1.0
 -- MySQL Workbench Forward Engineering
 
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
@@ -95,7 +94,8 @@ SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
-INSERT INTO `Administrador` (`Nombre`, `Email`, `Contraseña`) VALUES ('_TRIKITRAKATELAS', 'sanchez.cano.alejandro33@gmail.com', 'Triki' );
+INSERT INTO `Administrador` (`Apellido_Paterno`,`Apellido_Materno`,`Nombre`, `Contraseña`) VALUES ('Sanche','Cano','_TRIKITRAKATELAS','Triki' );
+INSERT INTO `Empleado` (`Apellido_Paterno`,`Apellido_Materno`,`Nombre`, `Contraseña`,`idAdministrador`) VALUES ('Titos','Vazquez','Carlos','123',1 );
 
 select * from `Marca`;
 describe `Marca`;
